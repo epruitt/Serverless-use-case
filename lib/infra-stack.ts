0f2fcfaed9e53359095f83e4aeeff0ba2c3f7b8c
epruitt <epruitt223@gmail.com>
2026-04-28 22:53:21 -0500

    add IAM Role for Lambda function  to access S3 bucket

--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -1,22 +1,26 @@
 import * as cdk from "aws-cdk-lib/core";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
     //S3 bucket
     const balanceStatusBucket = new s3.Bucket(this, "s3BucketLogicalId", {
       bucketName: "balancestatus-0127",
     });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // IAM Role
+    const iamBalanceStatusRole = new iam.Role(this, "iamBalanceRole", {
+      roleName: "bankingLambdaRole",
+      description: "role for lambda to access S3 bucket",
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+    iamBalanceStatusRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess"),
+    );
   }
 }
